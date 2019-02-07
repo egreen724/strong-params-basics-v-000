04d8881516cebdcb03ae3aae6e0d9da7bef8225d
Erin Green <erin.y.green@gmail.com>
2019-02-07 02:43:38 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,7 +18,7 @@ class PostsController < ApplicationController
 	end
 
 	def update
-		binding.pry 
+		binding.pry
 	  @post = Post.find(params[:id])
 	  @post.update(post_params(:title))
 	  redirect_to post_path(@post)
